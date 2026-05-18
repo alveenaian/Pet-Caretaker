6b447cc17cd8859ea5d751519487acb970270360
alveenaian <alvina.khachikyan@icloud.com>
2026-05-18 22:35:06 +0400

    Refactor pet actions to use addLog for output

--- script.js
+++ script.js
@@ -4,52 +4,52 @@ class Pet {
     this.type = type;
     this.hungry = true;
     this.energy = 50;
+
+    this.update();
   }
 
   feed() {
-    console.log(`${this.name} is eating...`);
+    addLog(this.name + " is eating...");
 
     setTimeout(() => {
       this.hungry = false;
       this.energy += 20;
-      console.log(`${this.name} finished eating!`);
+
+      this.update();
+
+      addLog(this.name + " finished eating!");
     }, 2000);
   }
 
   play() {
-    console.log(`${this.name} is playing...`);
+    addLog(this.name + " is playing...");
 
     setTimeout(() => {
-      this.energy -= 15;
       this.hungry = true;
-      console.log(`${this.name} got tired after playing.`);
+      this.energy -= 15;
+
+      this.update();
+
+      addLog(this.name + " got tired.");
     }, 2000);
   }
 
-  status() {
-    console.log(
-      `${this.name} | Energy: ${this.energy}% | Hungry: ${this.hungry}`
-    );
+  update() {
+    document.getElementById("hungryStatus").innerText =
+      "Hungry: " + this.hungry;
+
+    document.getElementById("energyText").innerText =
+      "Energy: " + this.energy + "%";
+
+    document.getElementById("energyFill").style.width =
+      this.energy + "%";
   }
+}
+
+function addLog(text) {
+  document.getElementById("log").innerHTML += "<p>" + text + "</p>";
 }
 
 const pet = new Pet("Buddy", "Dog");
 
-pet.status();
-
-// feed after 1 second
-setTimeout(() => {
-  pet.feed();
-}, 1000);
-
-// play after 5 seconds
-setTimeout(() => {
-  pet.play();
-}, 5000);
-
-// final status after 8 seconds
-setTimeout(() => {
-  pet.status();
-}, 8000);
-
-console.log("Pet simulation running with timers...");
+addLog("Pet simulator started!");
